2e8645a4c0159360d8f5a215c2172f58c996c2f5
AriPerkkio <ari.perkkio@gmail.com>
2019-07-24 20:03:00 +0300

    WIP

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-    "prettier/react"
-  ],
-  plugins: ["@typescript-eslint", "prettier"]
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'prettier/react',
+        'react-app',
+    ],
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': [
+            'warn',
+            {
+                allowExpressions: true,
+                allowTypedFunctionExpressions: true,
+            },
+        ],
+    },
 };
--- packages/client/src/components/Button.tsx
+++ packages/client/src/components/Button.tsx
@@ -1,20 +1,31 @@
 import React, { useReducer } from 'react';
+import classNames from 'classnames';
 
-export interface IButtonProps {
-  children?: React.ReactNode,
-  onClick?: (e: React.SyntheticEvent) => void
+export interface ButtonProps {
+    children?: React.ReactNode;
+    className?: string;
+    onClick?: (e: React.SyntheticEvent) => void;
 }
 
-const Button: React.SFC<IButtonProps> = (props) => {
-    return (
-  <button onClick={props.onClick} type="button">
-    {props.children}
-  </button>
-)};
+const Button: React.SFC<ButtonProps> = ({ onClick, className, ...props }) => {
+    const [clicked, toggle] = useReducer(s => !s, false);
 
-Button.defaultProps = {
-  children: null,
-  onClick: () => {}
+    const _onClick = (event: React.SyntheticEvent): void => {
+        toggle({});
+        onClick && onClick(event);
+    };
+
+    const _className = classNames('button', className, { clicked });
+
+    return (
+        <button
+            onClick={_onClick}
+            className={_className}
+            type='button'
+            {...props}>
+            {props.children}
+        </button>
+    );
 };
 
 export default Button;
