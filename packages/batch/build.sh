bfc78440957a10f8ed8f3a6d8dd282da6493edc6
AriPerkkio <ari.perkkio@gmail.com>
2019-09-13 17:42:57 +0300

    Docker image to ECR push script added

--- packages/batch/build.sh
+++ packages/batch/build.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+[[ "$(<cf-values.yml)" =~ (ECRURI: ([^[:space:]]*)) ]]
+ECR_URI=${BASH_REMATCH[2]}
+
+[[ "$(<cf-values.yml)" =~ (Region: ([^[:space:]]*)) ]]
+REGION=${BASH_REMATCH[2]}
+
 EXISTING_IMAGE_ID=$(docker images -q scrpr-batch)
 
 if [[ $EXISTING_IMAGE_ID != "" ]]; then
   echo 'Deleting existing image'
   docker rmi $EXISTING_IMAGE_ID
+  docker rmi ${ECR_URI}
   echo 'Delete complete'
 fi
 
 docker build -t scrpr-batch .
 
 if [[ $1 == "deploy" ]]; then
-    echo 'TODO deploy to ECR'
+    echo 'Preparing to push image to ECR'
+    docker tag scrpr-batch ${ECR_URI}
+    aws ecr get-login --region $REGION --no-include-email
+    docker push ${ECR_URI}
+    echo 'Image pushed to ECR'
 fi
