984e81a2c215944dedba2d9591466c94c7285824
AriPerkkio <ari.perkkio@gmail.com>
2019-09-22 20:36:31 +0300

    Batch working, initial setup

--- packages/batch/undeploy.sh
+++ packages/batch/undeploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo 'TODO delete docker image from ECR and use this in package.json scripts'
+[[ "$(<cf-values.yml)" =~ (Region: ([^[:space:]]*)) ]]
+REGION=${BASH_REMATCH[2]}
+
+# TODO remove all images
+
+echo Deleting image
+aws ecr batch-delete-image --repository-name scrpr-repository --region $REGION --image-ids imageTag=latest
+echo Image deleted
+
+echo Remaining images:
+aws ecr list-images --repository-name scrpr-repository --region $REGION
