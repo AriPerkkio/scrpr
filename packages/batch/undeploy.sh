f09a0230f39dc9420763ed18d70d7f92fec39dfc
AriPerkkio <ari.perkkio@gmail.com>
2019-09-22 21:00:44 +0300

    Batch working, initial setup

--- packages/batch/undeploy.sh
+++ packages/batch/undeploy.sh
@@ -3,8 +3,6 @@
 [[ "$(<cf-values.yml)" =~ (Region: ([^[:space:]]*)) ]]
 REGION=${BASH_REMATCH[2]}
 
-# TODO remove all images
-
 echo Deleting image
 aws ecr batch-delete-image --repository-name scrpr-repository --region $REGION --image-ids imageTag=latest
 echo Image deleted
